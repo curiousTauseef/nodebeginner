ef8d8e0156595036b6e0fbc2216bb91588787846
lpatmo <linda.peng@alumni.duke.edu>
2014-10-11 11:15:23 -0400

    page 35

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,14 +2,24 @@ var exec = require("child_process").exec;
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
-	// var content = 'empty';
 
-	exec("ls -lah", function(error, stdout, stderr){
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout)
+	var body = '<html>'+
+		'<head>' + 
+		'<meta http-equiv="Content-Type" content="text/html;'+
+		'charset=UTF-8"/>' +
+		'</head>' +
+		'<body>'+
+		'<form action="/upload" method="post">'+
+		'<textarea name="text" rows="20" cols="60"></textarea>'+
+		'<input type="submit" value="Submit text"/>'+
+		'</form>'+
+		'</body>'+
+		'</html>';
+
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.write(body);
 		response.end();
-	});
-	// return content;
+
 }	
 
 function upload(response) {
@@ -17,15 +27,8 @@ function upload(response) {
 	response.writeHead(200, {"Content-Type":"text/plain"});
 	response.write("Hello Upload");
 	response.end();
-	// return "Hello Upload!";
 }
 
 exports.start = start;
 exports.upload = upload;
 
-// function sleep(milliSeconds) {
-// 		var startTime = new Date().getTime();
-// 		while (new Date().getTime() < startTime + milliSeconds);
-// 	}
-// sleep(10000);
-// 	return "Hello Start!";
